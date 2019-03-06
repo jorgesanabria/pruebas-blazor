3765d9c0466f4ebd27d6df169a712624366c20ef
jorge sanabria <jorge.adrian.sanabria@gmail.com>
2019-03-06 17:44:30 -0300

    firts

---
+++ PruebaBlazor/Base/CivilizationsBase.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Services;
+using PruebaBlazor.Models;
+
+namespace PruebaBlazor.Base
+{
+    public class CivilizationsBase : ComponentBase
+    {
+        [Inject]
+        public HttpClient http { get; set; }
+        [Inject]
+        public IUriHelper url { get; set; }
+        public List<Civilization> Civilizations { get; set; }
+        protected override async Task OnInitAsync()
+        {
+            var r = await http.GetJsonAsync<Civilitations>("https://age-of-empires-2-api.herokuapp.com/api/v1/civilizations");
+            Civilizations = r.civilizations.ToList();
+        }
+        protected void IrAlInicio()
+        {
+            url.NavigateTo("/");
+        }
+    }
+}
---
+++ PruebaBlazor/Models/Civilization.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace PruebaBlazor.Models
+{
+    public class Civilitations
+    {
+        public List<Civilization> civilizations { get; set; }
+        public Civilitations()
+        {
+            civilizations = new List<Civilization>();
+        }
+    }
+    public class Civilization
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string expansion { get; set; }
+        public string army_type { get; set; }
+        public string[] unique_unit { get; set; }
+        public string[] unique_tech { get; set; }
+        public string team_bonus { get; set; }
+        public string[] civilization_bonus { get; set; }
+    }
+
+}
